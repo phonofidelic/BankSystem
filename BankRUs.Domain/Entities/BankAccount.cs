45dfe4ef2f81e1d9bed2123625246112d5ec82ac
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-27 10:14:24 +0100

    create Customer and BankAccount entities

---
+++ BankRUs.Domain/Entities/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankRUs.Domain.Entities
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public decimal Balance { get; set; }
+        // ToDo:
+        // Transactions
+    }
+}
---
+++ BankRUs.Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankRUs.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public required Guid ApplicationUserId { get; init; }
+        public ICollection<BankAccount> BankAccounts { get; set; } = [];
+    }
+}
