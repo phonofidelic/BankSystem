d2ba6beff02d9be911936edf1c514dcd70212842
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-01-30 16:20:43 +0100

    add Email service

---
+++ BankRUs.Intrastructure/Services/Email/FakeEmailSender.cs
@@ -0,0 +1,19 @@
+﻿using BankRUs.Application.Services.Email;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace BankRUs.Intrastructure.Services.Email
+{
+    public class FakeEmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string to, string from, string subject, string body)
+        {
+            var smtpClient = new SmtpClient("localhost", 25);
+
+            await smtpClient.SendMailAsync(
+                new MailMessage(from, to, subject, body));
+        }
+    }
+}
