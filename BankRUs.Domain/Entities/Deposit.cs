8f32f8ecc68a5076932ab9012e07b511928a2699
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-02-02 16:47:28 +0100

    add initial transaction entities

---
+++ BankRUs.Domain/Entities/Deposit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankRUs.Domain.Entities
+{
+    internal class Deposit : Transaction
+    {
+        public Deposit() : base(TransactionType.Deposit) { }
+    }
+}
