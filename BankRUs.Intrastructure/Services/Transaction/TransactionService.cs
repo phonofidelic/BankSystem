520ecdc26421cb4d6b909bbe7bcb4ffacf1b1528
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-02-10 19:00:35 +0100

    remove unused code

--- BankRUs.Intrastructure/Services/Transaction/TransactionService.cs
+++ BankRUs.Intrastructure/Services/Transaction/TransactionService.cs
@@ -1,19 +1,14 @@
-﻿using BankRUs.Application;
-using BankRUs.Application.Exceptions;
-using BankRUs.Application.Pagination;
+﻿using BankRUs.Application.Pagination;
 using BankRUs.Application.Services.TransactionService;
 using BankRUs.Domain.Entities;
-using BankRUs.Domain.ValueObjects;
 using BankRUs.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace BankRUs.Infrastructure.Services.TransactionService
 {
-    public class TransactionService(ApplicationDbContext context, IOptions<AppSettings> appSettings) : ITransactionService
+    public class TransactionService(ApplicationDbContext context) : ITransactionService
     {
         private readonly ApplicationDbContext _context = context;
-        private readonly AppSettings _appSettings = appSettings.Value;
         public async Task<CreateTransactionResult> CreateTransactionAsync(CreateTransactionRequest request)
         {
             Transaction transaction = new(request.Type) { 
@@ -29,18 +24,18 @@ namespace BankRUs.Infrastructure.Services.TransactionService
             return new CreateTransactionResult(transaction);
         }
 
-        public async Task<decimal> GetBalanceAfter(Guid bankAccountId, Guid transactionId)
-        {
-            var targetTransaction = await _context.Transactions.FindAsync(transactionId) ?? throw new TransactionNotFoundException();
+        //public async Task<decimal> GetBalanceAfter(Guid bankAccountId, Guid transactionId)
+        //{
+        //    var targetTransaction = await _context.Transactions.FindAsync(transactionId) ?? throw new TransactionNotFoundException();
             
-            return await _context.Transactions
-                .Where(t => t.BankAccountId == bankAccountId)
-                .OrderBy(t => t.CreatedAt)
-                .Where(t => t.BalanceAfter == null)
-                .Where(t => t.CreatedAt <= targetTransaction.CreatedAt)
-                .Where(t => t.Type == TransactionType.Deposit)
-                .SumAsync(t => t.Value);
-        }
+        //    return await _context.Transactions
+        //        .Where(t => t.BankAccountId == bankAccountId)
+        //        .OrderBy(t => t.CreatedAt)
+        //        .Where(t => t.BalanceAfter == null)
+        //        .Where(t => t.CreatedAt <= targetTransaction.CreatedAt)
+        //        .Where(t => t.Type == TransactionType.Deposit)
+        //        .SumAsync(t => t.Value);
+        //}
 
         public Task<decimal> GetBalanceAfterAsync(Guid bankAccountId, Guid transactionId)
         {
