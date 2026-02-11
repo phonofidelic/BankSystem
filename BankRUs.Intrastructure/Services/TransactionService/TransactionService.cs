f29069362ebdd604d0e68aa463aeca3adef21c83
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-02-11 12:01:44 +0100

    remove unused code

--- BankRUs.Intrastructure/Services/TransactionService/TransactionService.cs
+++ BankRUs.Intrastructure/Services/TransactionService/TransactionService.cs
@@ -24,46 +24,10 @@ namespace BankRUs.Infrastructure.Services.TransactionService
             return new CreateTransactionResult(transaction);
         }
 
-        //public async Task<decimal> GetBalanceAfter(Guid bankAccountId, Guid transactionId)
-        //{
-        //    var targetTransaction = await _context.Transactions.FindAsync(transactionId) ?? throw new TransactionNotFoundException();
-            
-        //    return await _context.Transactions
-        //        .Where(t => t.BankAccountId == bankAccountId)
-        //        .OrderBy(t => t.CreatedAt)
-        //        .Where(t => t.BalanceAfter == null)
-        //        .Where(t => t.CreatedAt <= targetTransaction.CreatedAt)
-        //        .Where(t => t.Type == TransactionType.Deposit)
-        //        .SumAsync(t => t.Value);
-        //}
-
         public Task<decimal> GetBalanceAfterAsync(Guid bankAccountId, Guid transactionId)
         {
             throw new NotImplementedException();
         }
-
-        //public async Task<BasePagedResult<Transaction>> GetTransactionsAsPagedResultAsync(TransactionsPageQuery query)
-        //{
-        //    var transactions = _context.Transactions.AsQueryable();
-
-        //    if (query.BankAccountId != null)
-        //        transactions = transactions.Where(t => t.BankAccountId == query.BankAccountId);
-
-        //    transactions = query.SortOrder == SortOrder.Ascending 
-        //        ? transactions.OrderBy(t => t.CreatedAt)
-        //        : transactions.OrderByDescending(t => t.CreatedAt);
-
-        //    if (query.StartPeriodUtc != null)
-        //        transactions = transactions.Where(t => t.CreatedAt >= query.StartPeriodUtc);
-
-        //    if (query.EndPeriodUtc != null)
-        //        transactions = transactions.Where(t => t.CreatedAt <= query.EndPeriodUtc);
-
-        //    if (query.Type != null)
-        //        transactions = transactions.Where(t => t.Type == query.Type);
-
-        //    return PaginationService.GetPagedResult(query, transactions);
-        //}
 
         public async Task<IQueryable<Transaction>> GetTransactionsAsync(TransactionsPageQuery query)
         {
