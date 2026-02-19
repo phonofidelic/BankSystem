87942f39a94e0d099139aefa7319a55e3afb1d2d
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2026-02-19 16:47:09 +0100

    add test for BankAccountNotFoundException

--- BankRUs.Application.Tests/Unit/UseCases/MakeDepositToBankAccountUnitTest.cs
+++ BankRUs.Application.Tests/Unit/UseCases/MakeDepositToBankAccountUnitTest.cs
@@ -1,3 +1,4 @@
+using BankRUs.Application.Exceptions;
 using BankRUs.Application.Tests.Infrastructure.Stubs;
 using BankRUs.Application.UseCases.MakeDepositToBankAccount;
 using BankRUs.Domain.Entities;
@@ -9,6 +10,7 @@ public class MakeDepositToBankAccountUnitTest
     [Fact]
     public async Task MakeDepositToBankAccountHandler_WhenBankAccountExists_ShouldReturnResult()
     {
+        // Arrange:
         var customerId = Guid.NewGuid();
         var bankAccountId = Guid.NewGuid();
 
@@ -38,5 +40,37 @@ public class MakeDepositToBankAccountUnitTest
         Assert.Equal((decimal)100.25, makeDepositResult.Amount);
         Assert.Equal("SEK", makeDepositCommand.Currency);
         Assert.Equal("Test make deposit transaction", makeDepositResult.Reference);
+    }
+
+    [Fact]
+    public async Task MakeDepositToBankAccountHandler_WhenBankAccountNotFound_ShouldThrowNotFound()
+    {
+        // Arrange:
+        var customerId = Guid.NewGuid();
+
+        var makeDepositHandler = new MakeDepositToBankAccountHandler(
+            unitOfWork: new UnitOfWorkStub(),
+            bankAccountsRepository: new BankAccountRepositoryStub(customerId),
+            currencyService: new CurrencyServiceStub(),
+            transactionService: new TransactionServiceStub(),
+            auditLogger: new AuditLoggerStub()
+        );
+
+        var makeDepositCommand = new MakeDepositToBankAccountCommand(
+            CustomerId: customerId,
+            BankAccountId: Guid.NewGuid(),
+            Amount: (decimal)100.25111,
+            Currency: "SEK",
+            Reference: "Test bank account not found deposit transaction");
+
+        // Act:
+        try
+        {
+            await makeDepositHandler.HandleAsync(makeDepositCommand);
+        }
+        catch (Exception ex) {
+            // Assert:
+            Assert.Equal(new BankAccountNotFoundException(), ex);
+        }
     }
 }
