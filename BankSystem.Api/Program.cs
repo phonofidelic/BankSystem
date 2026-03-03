a15fb53ca31023d418f326b3f7e1339553f1ea93
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-03 18:06:39 +0100

    apply pending migrations in prod

--- BankSystem.Api/Program.cs
+++ BankSystem.Api/Program.cs
@@ -158,16 +158,15 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
+// Apply any pending migrations
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+await dbContext.Database.MigrateAsync();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
-
-    using var scope = app.Services.CreateScope();
-
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    dbContext.Database.Migrate();
 
     await IdentitySeeder.SeedAsync(scope.ServiceProvider);
     await CurrencySeeder.SeedAsync(scope.ServiceProvider);
